f3c3908ed75f4645556e287a7994b1acbeb00871
Alex Antsiferov <wetterkrank@gmail.com>
2020-10-26 20:46:26 +0000

    Subscription enabled by default

--- dasbot/chat.py
+++ dasbot/chat.py
@@ -9,7 +9,7 @@ log = logging.getLogger(__name__)
 
 
 class Chat(object):
-    def __init__(self, chat_id, subscribed=False, quiz_time="12:00", last_seen=None, quiz=Quiz()):
+    def __init__(self, chat_id, subscribed=True, quiz_time="12:00", last_seen=None, quiz=Quiz()):
         self.id = chat_id
         self.subscribed = subscribed
         self.quiz_time = quiz_time
@@ -24,7 +24,7 @@ class ChatSchema(Schema):
     class Meta:
         unknown = EXCLUDE   # Skip unknown fields on deserialization
     chat_id = fields.Integer()
-    subscribed = fields.Boolean(missing = False)
+    subscribed = fields.Boolean(missing = True)
     quiz_time = fields.String(missing = "12:00")
     last_seen = fields.DateTime(missing = None)
     quiz = fields.Nested(QuizSchema)
