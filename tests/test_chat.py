0f2bfd06a194be145bf08c6dd0d92135a97d083d
Aleksandr Antciferov <wetterkrank@gmail.com>
2025-02-22 21:12:38 +0100

    for new chats, use current day time for subscription

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -9,14 +9,15 @@ FAKE_NOW = datetime.fromisoformat('2020-11-01 11:05:00+00:00')
 
 
 class TestChat(unittest.TestCase):
-    @patch('dasbot.util.random_hhmm', return_value='11:15')
     @patch('dasbot.models.chat.datetime')
-    def test_init(self, mock_dt, _mock_random_hhmm):
-        mock_dt.now().astimezone = MagicMock(return_value=FAKE_NOW)
+    @patch('dasbot.util.datetime')
+    def test_init(self, mock_chat_dt, mock_util_dt):
+        mock_chat_dt.now = MagicMock(return_value=FAKE_NOW)
+        mock_util_dt.now = MagicMock(return_value=FAKE_NOW)
         chat = Chat(chat_id=1001)
         self.assertEqual(1001, chat.id)
-        # For new chats, next scheduled quiz will run on random time tomorrow
-        self.assertEqual(chat.quiz_scheduled_time, datetime.fromisoformat('2020-11-02 11:15:00+00:00'))
+        # For new chats, next scheduled quiz will run on the same time tomorrow
+        self.assertEqual(chat.quiz_scheduled_time, datetime.fromisoformat('2020-11-02 11:05:00+00:00'))
         self.assertEqual(None, chat.last_seen)
 
     @patch('dasbot.models.chat.datetime')
